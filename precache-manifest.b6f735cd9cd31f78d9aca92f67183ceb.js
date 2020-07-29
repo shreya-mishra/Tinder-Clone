85c2a07e01d8523671558a2eeed66bfc1a7b4811
shreya-mishra <shreyamishra040398@gmail.com>
2020-07-29 13:51:16 +0530

    Updates

---
+++ precache-manifest.b6f735cd9cd31f78d9aca92f67183ceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "077585820bc8a8c64e212b3fcb7b71e2",
+    "url": "/shreya-mishra/Tinder-Clone.git/index.html"
+  },
+  {
+    "revision": "c670cfcd508ae02427b1",
+    "url": "/shreya-mishra/Tinder-Clone.git/static/css/main.c6b54b08.chunk.css"
+  },
+  {
+    "revision": "4a9c6178924287620e2c",
+    "url": "/shreya-mishra/Tinder-Clone.git/static/js/2.901c2cfd.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/shreya-mishra/Tinder-Clone.git/static/js/2.901c2cfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c670cfcd508ae02427b1",
+    "url": "/shreya-mishra/Tinder-Clone.git/static/js/main.1c7b03d0.chunk.js"
+  },
+  {
+    "revision": "19d9ac51b47549439b7f",
+    "url": "/shreya-mishra/Tinder-Clone.git/static/js/runtime-main.22e9ba6f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/shreya-mishra/Tinder-Clone.git/precache-manifest.b6f735cd9cd31f78d9aca92f67183ceb.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shreya-mishra/Tinder-Clone.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
